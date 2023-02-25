693eb7166ae7f62227ce4a802b9a492808d658a6
unknown <nancykumari620@gmail.com>
2023-02-25 10:02:31 +0530

    Routes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+import React from "react";
+const Navbar = () => {
+  return (
+    <div>
+      navbor
+    </div>
+  )
+}
+
+export default Navbar
