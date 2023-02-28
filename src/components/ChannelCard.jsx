75d71baa9b9f1497feeab87594021c987d1c8673
unknown <nancykumari620@gmail.com>
2023-02-28 12:07:12 +0530

    ChannelCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-
-const ChannelCard = () => {
-  return (
-    <div>
-      channel
-    </div>
-  )
-}
-
-export default ChannelCard
+import {Box,CardContent,CardMedia,Typography} from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { demoChannelUrl, demoProfilePicture,demoChannelTitle, demoVideoUrl } from '../utils/constants';
+const ChannelCard = ({channelDetail:{
+  snippet}}) => {
+    return (
+      <Box sx={{width:{md:'320px',xs:'100%'},
+      boxShadow:'none',borderRadius:12}}>
+          <Link to={snippet?.channelId?`/channel/${snippet?.channelId}`:
+          demoVideoUrl}>
+          <CardMedia 
+          image={snippet?.thumbnails?.medium?.url}
+          alt={snippet?.channelTitle}
+          sx={{width:319,height:180}}/>
+  </Link>
+  <CardContent sx={{backgroundColor:"white",
+  height:'106x'}}>
+    <Link to={snippet?.channelId?`/channel/${snippet?.ChannelCardchannelId}`:
+  demoChannelUrl}>
+    <Typography variant="subtitle"
+    fontWeight="bold" color="black">
+      {snippet?.title.slice(0,60)||
+      demoChannelTitle.slice(0,60)}
+    </Typography>
+    </Link>
+  </CardContent>
+      </Box>
+    )
+  }
+  
+  
+  export default ChannelCard
+  
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,7 +6,8 @@ import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannel
 const VideoCard = ({video:{id:{videoId},
 snippet}}) => {
   return (
-    <Card>
+    <Card sx={{width:{md:'320px',xs:'100%'},
+    boxShadow:'none',borderRadius:0}}>
         <Link to={videoId?`/video/${videoId}`:
         demoVideoUrl}>
         <CardMedia 
@@ -14,6 +15,26 @@ snippet}}) => {
         alt={snippet?.title}
         sx={{width:358,height:180}}/>
 </Link>
+<CardContent sx={{backgroundColor:"white",
+height:'106x'}}>
+  <Link to={videoId?`/video/${videoId}`:
+demoVideoUrl}>
+  <Typography variant='="subtitle'
+  fontWeight="bold" color="black">
+    {snippet?.title.slice(0,60)||
+    demoVideoTitle.slice(0,60)}
+  </Typography>
+  </Link>
+<Link to={snippet?.channelId?`/channel/${snippet?.channelId}`:demoChannelUrl}>
+  <Typography variant='subtitle2'
+  fontweight="bold" color="black">
+    {
+      snippet?.channelTitle||demoChannelTitle
+    }
+    <CheckCircle sx={{fontSize:12,color:'black',ml:'5px'}}/>
+  </Typography>
+</Link>
+</CardContent>
     </Card>
   )
 }
